e730379c23e8522c19209037a6092dfef4e75644
HackPai <123292385+HackPai@users.noreply.github.com>
2021-08-12 20:00:38 +0000

    Updating Mura.js

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -15,4 +15,4 @@ def random_predict(number:int=1) -> int:
     while True:
         count += 1
         predict_number = np.random.randint(1, 101) # предполагаемое число
-        if number == predict_numbe
+        if number == predict_number:
