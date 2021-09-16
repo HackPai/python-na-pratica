2f80b3651b3f24898f627c2784885178c7c4085d
HackPai <123292385+HackPai@users.noreply.github.com>
2021-09-16 09:50:33 +0000

    Update ignore.

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -16,4 +16,5 @@ def random_predict(number:int=1) -> int:
         count += 1
         predict_number = np.random.randint(1, 101) # предполагаемое число
         if number == predict_number:
-            break # выход из цикла, если угадал
+            break # выход из цикла, если угадали
+ 
