36afb4ffa01085f78f23f1de0fb2d46b850ff4f6
HackPai <123292385+HackPai@users.noreply.github.com>
2021-09-24 07:36:40 +0000

    Removed unused usings

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -17,4 +17,4 @@ def random_predict(number:int=1) -> int:
         predict_number = np.random.randint(1, 101) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
-    retu
+    retur
