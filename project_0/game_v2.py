e4d68c7bca4412a99b68c7e798f07a8c5c8ff94d
HackPai <123292385+HackPai@users.noreply.github.com>
2021-10-21 09:50:59 +0000

    v1.03

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -18,4 +18,4 @@ def random_predict(number:int=1) -> int:
         if number == predict_number:
             break # выход из цикла, если угадали
     return(count)
-def score_game(random_predict) -
+def score_game(random_predict) ->
