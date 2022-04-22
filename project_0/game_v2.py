57e778c0613faa876b5c4ada13e3e514df207086
HackPai <123292385+HackPai@users.noreply.github.com>
2022-04-22 16:52:12 +0000

    copy fix

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -25,4 +25,4 @@ def score_game(random_predict) -> int:
         random_predict ([type]): функция угадывания
 
     Returns:
-        int: среднее количество попы
+        int: среднее количество попыто
