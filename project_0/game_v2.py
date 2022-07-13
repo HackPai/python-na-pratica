349f6c772c8c08a861ff058a1cc8e92355c52cc7
HackPai <123292385+HackPai@users.noreply.github.com>
2022-07-13 15:03:49 +0000

    updated the version

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -29,4 +29,4 @@ def score_game(random_predict) -> int:
     """
 
     count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воcпр
+    np.random.seed(1) # фиксируем сид для воcпро
